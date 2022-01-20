21057a26a30a2a841f930dab8482caf0ddaae4a2
miloszwasacz <wasacz.dev@gmail.com>
2022-01-21 00:02:06 +0100

    Add puzzle generator

---
+++ scripts/generator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+code_file_path="./code-files/puzzle"
+template_html=template.html
+template_js=template.js
+output=../zagadki/pietro
+
+for i in 1 2 3 4
+do
+    code_file="${code_file_path}${i}.html"
+    . "${code_file_path}${i}.conf"
+    eval "echo \"$(cat "${template_html}" | sed 's/\"/\\\"/g')\"" > ${output}${i}.html
+    eval "echo \"$(cat "${template_js}" | sed 's/\"/\\\"/g')\"" > ${output}${i}.js
+done
